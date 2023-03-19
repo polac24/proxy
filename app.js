151e1d05c7bc4942f47989b8df7dfdcbe523b6c6
Bartosz Polaczyk <polac24@gmail.com>
2023-03-19 09:13:58 -0700

    Experiment

--- app.js
+++ app.js
@@ -42,6 +42,68 @@ app.post(['/proxy'], async (req, res, next) => {
     }
 })
 
+app.get(['*'], async (req, res, next) => {
+    try {
+        res.set('Content-Type', 'text/html');
+        query = req.query
+        // req.path
+        ref = req.headers.referrer || req.headers.referer
+        console.log(req.path)
+        console.log("Referrer:"+ ref)
+        console.log("query.url: "+query.url)
+        url = query.url
+        // use https on production
+        const sendindScheme = process.env.RENDER != 'true' ? "https" : "https"
+        const sendingHost = req.headers.host
+        headers = req.headers
+        const preamble = sendindScheme+"://"+sendingHost
+        delete headers['host']
+        delete headers['content-length']
+        if (!req.path.endsWith('.ts')) {
+            headers['Accept-Encoding'] = ''
+        }
+
+        if (req.path.endsWith('.ts')) {
+            console.log("patching1...")
+            url = 'https://kamery-szczawnica.poxi.pl/stream/promenada/'+req.path
+        }else if (!url && !!ref && ref.startsWith(preamble)) {
+            console.log("patching2...")
+            url = ref +"/"+ req.path
+        }
+        console.log("Final URL: "+url)
+        console.log("preamble: "+preamble)
+        // req.rawBody = '';
+            console.log(url)
+            try {
+                type = null
+                if (req.path.endsWith('.ts')) {
+                    type = 'arraybuffer'
+                }
+                const response = await axios.get(url, { headers: headers, responseType: type})
+                console.log(typeof(response.data))
+                response.headers['Access-Control-Allow-Origin'] = '*'
+                console.log(response.headers)
+                // res.set(response.headers)
+                console.log(response.data.length +" vs "+ response.headers['content-length'])
+                if (!req.path.endsWith('.ts')) {
+                    console.log("sending:"+response.data.replace("=\"./", "=\""+preamble+'/proxy?url=https://kamery-szczawnica.poxi.pl/'+"/"))
+                    res.send(response.data.replace("=\"./", "=\""+preamble+'/proxy?url=https://kamery-szczawnica.poxi.pl/'+"/"))
+                } else {
+                    res.send(response.data)
+                }
+            } catch (error) {
+                // Passes errors into the error handler
+                console.log(error)
+                return next(error)
+            }
+    } catch (error) {
+        // Passes errors into the error handler
+        console.log(error)
+        return next(error)
+    }
+})
+
+
 
 
 
@@ -49,6 +111,14 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 if (process.env.RENDER != 'true') {
     (async () => {
+        // const res = await axios.get('https://kamery-szczawnica.poxi.pl/kamera.php?kam=14', { headers: { 'Accept-Encoding': '' }});
+        // console.log(res.data.replace("=\"./", "=\""+'https://localhost:3011/proxy/'+"/"))
+        // const response = await axios.get('https://kamery-szczawnica.poxi.pl/kamera.php\?kam\=14' , { 'decompress': true })
+        // // console.log(response.data)
+        // zlib.gunzip(response.data, function (_err, output) {
+        //     console.log(output)
+        //     console.log(output.toString())
+        //   })
         // Make a probe request on local env
         // url = 'https://www.google.com/_/TravelFrontendUi/data/travel.frontend.flights.FlightsFrontendService/GetCalendarPicker?f.sid=-3480986134142297956&bl=boq_travel-frontend-ui_20221213.00_p2&hl=en&soc-app=162&soc-platform=1&soc-device=1&_reqid=3238195&rt=c'
         // const response = await axios.post(url)
